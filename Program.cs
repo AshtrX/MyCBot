710835c5e3d5c9f3f0dc077bd7d4bfbe0ce4a78b
AshtrX <65673398+AshtrX@users.noreply.github.com>
2023-04-14 04:57:20 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,37 +17,40 @@ namespace MonBotDiscord
         }
 
         public async Task MainAsync()
+{
+    _client = new DiscordSocketClient();
+    _client.Log += Log;
+
+    var commandService = new CommandService();
+    await commandService.AddModuleAsync<PingCommand>(null);
+    await commandService.AddModuleAsync<BanCommand>(null); // Ajoutez cette ligne
+
+    _client.MessageReceived += async (message) =>
+    {
+        // Ne traitez pas les messages des bots
+        if (message.Author.IsBot) return;
+
+        // Préfixe de la commande
+        char prefix = '!';
+
+        // Vérifiez si le message commence par le préfixe de la commande
+        if (message.Content.StartsWith(prefix))
         {
-            _client = new DiscordSocketClient();
-            _client.Log += Log;
+            int argPos = 1;
+            var context = new SocketCommandContext(_client, message as SocketUserMessage);
 
-            var commandService = new CommandService();
-            await commandService.AddModuleAsync<PingCommand>(null);
+            // Exécutez la commande
+            await commandService.ExecuteAsync(context, argPos, null);
+        }
+    };
 
-            _client.MessageReceived += async (message) =>
-            {
-                // Ne traitez pas les messages des bots
-                if (message.Author.IsBot) return;
+    // Remplacez YOUR_BOT_TOKEN par le token de votre bot
+    await _client.LoginAsync(TokenType.Bot, "YOUR_BOT_TOKEN");
+    await _client.StartAsync();
 
-                // Préfixe de la commande
-                char prefix = '!';
+    await Task.Delay(-1);
+}
 
-                // Vérifiez si le message commence par le préfixe de la commande
-                if (message.Content.StartsWith(prefix))
-                {
-                    int argPos = 1;
-                    var context = new SocketCommandContext(_client, message as SocketUserMessage);
-
-                    // Exécutez la commande
-                    await commandService.ExecuteAsync(context, argPos, null);
-                }
-            };
-
-            // Remplacez YOUR_BOT_TOKEN par le token de votre bot
-            await _client.LoginAsync(TokenType.Bot, "YOUR_BOT_TOKEN");
-            await _client.StartAsync();
-
-            await Task.Delay(-1);
         }
 
         private Task Log(LogMessage msg)
